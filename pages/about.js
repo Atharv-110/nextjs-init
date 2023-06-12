137234eaa37469f8f1f994e2a99c09f9e6a38bbf
Atharv Vani <atharvavani110@gmail.com>
2023-06-13 05:21:31 +0530

    next js is fun

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>NextJS-AV | About</title>
+        <meta name="keywords" content="Atharv" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ad, ea
+          animi totam eius aspernatur voluptas ipsum ut facilis temporibus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ad, ea
+          animi totam eius aspernatur voluptas ipsum ut facilis temporibus.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
